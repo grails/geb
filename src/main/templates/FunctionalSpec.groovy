f7ba3cae2666c204f59748fb3b6588e16dcd9669
graemerocher <graeme.rocher@gmail.com>
2015-03-04 10:51:20 +0100

    Rollback changes by default

--- src/main/templates/FunctionalSpec.groovy
+++ src/main/templates/FunctionalSpec.groovy
@@ -1,7 +1,7 @@
 package ${packageName}
 
 import grails.test.mixin.integration.Integration
-import grails.transaction.Transactional
+import grails.transaction.*
 
 import spock.lang.*
 import geb.spock.*
@@ -10,6 +10,7 @@ import geb.spock.*
  * See the API for {@link grails.test.mixin.support.GrailsUnitTestMixin} for usage instructions
  */
 @Integration
+@Rollback
 class ${className}Spec extends GebSpec {
 
     def setup() {
