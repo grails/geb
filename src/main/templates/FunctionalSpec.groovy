603d678dd75d00a5a95dbc4150043be88eac81eb
jameskleeh <james.kleeh@gmail.com>
2017-08-17 09:29:50 -0400

    Use correct integration test annotation in FunctionalSpec

--- src/main/templates/FunctionalSpec.groovy
+++ src/main/templates/FunctionalSpec.groovy
@@ -1,9 +1,8 @@
 package ${packageName}
 
-import grails.test.mixin.integration.Integration
+import grails.testing.mixin.integration.Integration
 import grails.transaction.*
 
-import spock.lang.*
 import geb.spock.*
 
 /**
