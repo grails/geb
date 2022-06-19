9c280b8a2ad45bd6b715e27870cf0d8f669c09de
Puneet Behl <behlp@objectcomputing.com>
2022-06-19 07:10:02 +0530

    Upgrade to Grails 5 (#12)

--- src/main/groovy/geb/GebGrailsPlugin.groovy
+++ src/main/groovy/geb/GebGrailsPlugin.groovy
@@ -7,33 +7,18 @@ import groovy.transform.CompileStatic
 @PluginSource
 @CompileStatic
 class GebGrailsPlugin extends Plugin {
-
-   // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "4.0.0 > *"
-    // resources that are excluded from plugin packaging
+    def grailsVersion = "5.1.0 > *"
     def pluginExcludes = [
-        "grails-app/views/error.gsp"
+            "grails-app/views/error.gsp"
     ]
-
-    
-    def title = "Geb Plugin" 
+    def title = "Grails Geb Plugin"
     def author = "Graeme Rocher"
     def authorEmail = "grocher@pivotal.io"
     def description = '''\
 Plugin that adds Geb functional testing code generation features.
 '''
-
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/geb"
-
-    // Extra (optional) plugin metadata
-
-    // License: one of 'APACHE', 'GPL2', 'GPL3'
+    def documentation = "https://plugins.grails.org/plugin/grails/geb"
     def license = "APACHE"
-
-    // Location of the plugin's issue tracker.
-    def issueManagement = [ system: "Github Issues", url: "http://github.com/grails3-plugins/geb/issues" ]
-
-    // Online location of the plugin's browseable source code.
-    def scm = [ url: "http://github.com/grails3-plugins/geb" ]
+    def issueManagement = [system: "Github Issues", url: "https://github.com/grails/geb/issues"]
+    def scm = [url: "https://github.com/grails/geb"]
 }
